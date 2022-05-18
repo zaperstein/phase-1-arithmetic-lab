bfd4930585ca3d6d3dfb992d65e51579d3c26e68
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-05-18 10:22:13 -0500

    commit tree 79c81d43435d754b6451e45c6febb29185b327ff

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+const random = Math.ceil(Math.random());
+//but the random number will always be 1... no? 
+
+function mod(num3, num4) {
+  (num3%num4) === 4;
+}
+
+// function max(arr)
